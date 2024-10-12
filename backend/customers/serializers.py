d6e7699ad35d21cf59edad3dbc313c52aea3da29
Chelsea Jaculina <chelseajaculina@gmail.com>
2024-10-12 02:39:49 -0700

    Delete backend directory

--- backend/customers/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import CustomerProfile
-
-class CustomerProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomerProfile
-        fields = ['username', 'email', 'nickname', 'date_of_birth', 'city', 'state', 'country', 'phone_number', 'profile_picture', 'favorites']
